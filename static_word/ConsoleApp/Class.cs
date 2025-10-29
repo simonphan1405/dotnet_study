9f50ddd7354b81039cd3e734963b5d76fda604d2
hieuphan <p-thanhhieu@pasonatech.vn>
2025-10-29 10:26:25 +0700

    static: create normal variable

---
+++ static_word/ConsoleApp/Class.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace ConsoleApp;
+
+public class Class
+{
+    public int x;
+}
---
+++ static_word/ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp;
+
+var c1 = new Class() { x = 10 };
+var c2 = new Class() { x = 20 };
+
+Console.WriteLine(c1.x);
+Console.WriteLine(c2.x);
+
+c2.x = 30;
+
+Console.WriteLine(c1.x);
+Console.WriteLine(c2.x);
